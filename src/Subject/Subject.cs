5b6eb2ed7567a78ba0cf7df2a2de343b65f8b419
penVqen <159798395+penVqen@users.noreply.github.com>
2024-12-25 10:23:08 +0300

    Добавлен проект с созданным классом Subject.

---
+++ src/Subject/Subject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Subject
+{
+    class Subject
+    {
+        // Название
+        public string Name;
+
+        // Семестр
+        public int Semester;
+
+        // Форма аттестации
+        public string FormCertification;
+
+        public Subject () { }
+        public Subject(string name, int semester, string formCertification)
+        {
+            Name = name;
+            Semester = semester;
+            FormCertification = formCertification;
+        }
+    }
+}
