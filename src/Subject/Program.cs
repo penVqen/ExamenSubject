e4e3aa433e536d38025d200a663edc1e57ee4e3a
penVqen <159798395+penVqen@users.noreply.github.com>
2024-12-25 11:07:52 +0300

    Реализована главная функция.

--- src/Subject/Program.cs
+++ src/Subject/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Subject
 {
@@ -6,7 +6,34 @@ namespace Subject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите количество предметов:\n");
+            int size = int.Parse(Console.ReadLine());
+
+            SubjectControl control = new SubjectControl(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Введите название предмета номер {i+1}");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Введите семестр, в котором проводится предмет");
+                int semester = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите форму аттестации (Устная, Письменная, Смешанная)");
+                string formCertification = Console.ReadLine();
+
+                control.AddSubject(new Subject(name, semester, formCertification));
+            }
+
+            control.SortSubject();
+
+            Console.WriteLine("Введите название файла, без расширение (по умолчанию .txt)");
+            string filename = Console.ReadLine() + ".txt";
+
+            Console.WriteLine("Введите путь сохранения файла, пример: D:\\Users\\stu-ipsp121\\Desktop\\subject");
+            string filepath = Console.ReadLine();
+
+            control.Save(filename, filepath);
         }
     }
 }
