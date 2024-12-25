542ce87ca022c302daa14bc887bfc560449eeab5
penVqen <159798395+penVqen@users.noreply.github.com>
2024-12-25 11:07:16 +0300

    Класс SubjectControl был перенесен в другую папку.

---
+++ src/Subject/SubjectControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Subject
+{
+    class SubjectControl
+    {
+        private Subject[] subjects;
+        private int subjectCount;
+
+        public SubjectControl(int size)
+        {
+            subjects = new Subject[size];
+            subjectCount = 0;
+        }
+
+        public void AddSubject(Subject subject)
+        {
+            if (subjectCount < subjects.Length)
+            {
+                subjects[subjectCount++] = subject;
+            }
+            else
+            {
+                Console.WriteLine("Массив переполнен");
+            }
+        }
+
+        public void SortSubject()
+        {
+            subjects = subjects
+                .OrderByDescending(f => f.FormCertification)
+                .ThenByDescending(n => n.Name)
+                .ToArray();
+        }
+
+        public void Save(string filename, string filepath)
+        {
+            string fullPath = Path.Combine(filename, filepath);
+            using (StreamWriter sw = new StreamWriter(fullPath))
+            {
+                foreach (Subject subject in subjects)
+                {
+                    sw.WriteLine($"Название: {subject.Name}, Семестр: {subject.Name}, Форма аттестации: {subject.FormCertification}");
+                }
+            }
+        }
+    }
+}
